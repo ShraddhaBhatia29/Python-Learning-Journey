b35f35f60e7b94deecd455463e21698a3c01736e
ShraddhaBhatia29 <shraddhaGithub29@gmail.com>
2026-05-06 16:48:28 +0530

    Add variable and string manipulation examples

---
+++ variables_and_strings.py
@@ -0,0 +1,10 @@
+a = 3
+print(a)
+print(type(a))
+
+print("{} {}".format("value of a is", a))
+print(f"Value of a is {a}")
+
+name = "tina"
+print(f"my name is {name}")
+print("my name is " + name)  # string concatenation
